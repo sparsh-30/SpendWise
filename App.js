de28c384159aa03099e07b48aa70e8d62f367ed1
Sparsh Gupta <gsparsh04@gmail.com>
2023-10-06 19:35:33 +0530

    Project Initialized

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Text className="text-5xl text-red-500">App</Text>
+    </NavigationContainer>
+  )
+}
